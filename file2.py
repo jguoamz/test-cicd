2f802104172437a2bc5af89ce58fcb3bb9355c21
Jason Guo <81202082+jguoamz@users.noreply.github.com>
2022-01-07 17:05:27 -0800

    Update file2.py

--- file2.py
+++ file2.py
@@ -1,17 +1,18 @@
 def foo(a_list):
   a_list.sort(key=lambda x: x.idx)
-  print("hello")
-  print("world")
   current_idx = None
   current_list = []
   for a in a_list:
-    if a.idx == -1:
-      continue
+    current_idx = a.idx
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
     if current_idx is None:
       current_idx = a.idx
-      current_list.append(a)
       continue
-    if anomaly.idx == current_idx:
+    if a.idx == current_idx:
       current_list.append(a)
 
   return current_list
