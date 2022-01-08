b8e9f3a31254be540259dbc206d5b34d4ad878d5
Jason Guo <81202082+jguoamz@users.noreply.github.com>
2022-01-07 16:15:30 -0800

    Create file4.py

---
+++ file4.py
@@ -0,0 +1,21 @@
+def foo(x) -> Tuple:
+  var1 = "foo"
+  var2 = "foo"
+  var3 = "foo"
+  var4 = "foo"
+  var5 = "foo"
+  if x is None:
+    var1 = "bar"
+    var2 = "bar"
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  print("hello")
+  
+  return var1, var2, var3, var4, 
