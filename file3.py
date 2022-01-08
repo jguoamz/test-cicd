9482e862ec92d2de4451234ccb1c2d1729637b64
Jason Guo <81202082+jguoamz@users.noreply.github.com>
2022-01-07 17:50:11 -0800

    Create file3.py

---
+++ file3.py
@@ -0,0 +1,19 @@
+def foo(x):
+  y = x.f
+  print("hello")
+  print("dummy statement")
+  """
+  random comments
+  foo
+  bar
+  x
+  y
+  z
+  a
+  b
+  c
+  """
+  if y == None:
+    return 1
+  else:
+    return y
